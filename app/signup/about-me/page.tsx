bea159ce9da9f714ff084f0321a21f43e970d5bc
asuna <cherepanova.so04@gmail.com>
2025-12-05 23:25:01 +0500

    chore: bg for pages

--- app/signup/about-me/page.tsx
+++ app/signup/about-me/page.tsx
@@ -22,65 +22,67 @@ export default function AboutMePage() {
   };
 
   return (
-    <section className='h-[100vh] px-4 pt-6 pb-10 bg-(--color-white)'>
-      <nav className='flex align-center justify-between mb-5'>
-        <Link href='/'>
-          <ChevronLeft />
-        </Link>
-        <Logo width={56} height={50} />
-      </nav>
-
-      <header className='flex flex-col gap-3 mb-5'>
-        <h1 className='font-medium text-2xl leading-[1.2] text-center align-middle text-(--color-black)'>
-          Личная информация
-        </h1>
-        <p className='font-normal text-[18px] leading-[1.2] text-center text-(--color-black)'>
-          Пожалуйста, заполните данные
-        </p>
-      </header>
-
-      <form onSubmit={submit} className='flex flex-col h-full'>
-        <div className='flex flex-col gap-2'>
-          <Input
-            id='name'
-            name='name'
-            label='Введите имя'
-            value={formData.name}
-            onChange={(e) =>
-              setFormData((prev) => ({ ...prev, name: e.target.value }))
-            }
-            required
-          />
-          <Input
-            id='username'
-            name='username'
-            label='Придумайте никнейм'
-            value={formData.username}
-            onChange={(e) =>
-              setFormData((prev) => ({ ...prev, username: e.target.value }))
-            }
-            required
-          />
-        </div>
-
-        <p className='font-medium text-[12px] leading-[1.2] text-(--color-gray) mb-4 mt-auto'>
-          Нажимая на «Зарегистрироваться», вы соглашаетесь с 
-          <Link href='/' className='text-(--color-primary)'>
-            Пользовательским соглашением
+    <main className="bg-[url('/Background.png')] bg-cover bg-center h-screen w-full grid">
+      <section className='m-8 px-4 pt-6 pb-10 bg-(--color-white) rounded-lg flex flex-col gap-5'>
+        <nav className='flex align-center justify-between'>
+          <Link href='/'>
+            <ChevronLeft />
           </Link>
-          .
-        </p>
+          <Logo width={56} height={50} />
+        </nav>
 
-        <Button
-          variant={
-            !formData.name || !formData.username ? 'disabled' : 'primary'
-          }
-          full={true}
-          type='submit'
-          children='Зарегистрироваться'
-          size='lg'
-        ></Button>
-      </form>
-    </section>
+        <header className='flex flex-col gap-3'>
+          <h1 className='font-medium text-2xl leading-[1.2] text-center align-middle text-(--color-black)'>
+            Личная информация
+          </h1>
+          <p className='font-normal text-[18px] leading-[1.2] text-center text-(--color-black)'>
+            Пожалуйста, заполните данные
+          </p>
+        </header>
+
+        <form onSubmit={submit} className='flex flex-col h-full'>
+          <div className='flex flex-col gap-2'>
+            <Input
+              id='name'
+              name='name'
+              label='Введите имя'
+              value={formData.name}
+              onChange={(e) =>
+                setFormData((prev) => ({ ...prev, name: e.target.value }))
+              }
+              required
+            />
+            <Input
+              id='username'
+              name='username'
+              label='Придумайте никнейм'
+              value={formData.username}
+              onChange={(e) =>
+                setFormData((prev) => ({ ...prev, username: e.target.value }))
+              }
+              required
+            />
+          </div>
+
+          <p className='font-medium text-[12px] leading-[1.2] text-(--color-gray) mb-4 mt-auto'>
+            Нажимая на «Зарегистрироваться», вы соглашаетесь с 
+            <Link href='/' className='text-(--color-primary)'>
+              Пользовательским соглашением
+            </Link>
+            .
+          </p>
+
+          <Button
+            variant={
+              !formData.name || !formData.username ? 'disabled' : 'primary'
+            }
+            full={true}
+            type='submit'
+            children='Зарегистрироваться'
+            size='lg'
+          ></Button>
+        </form>
+      </section>
+    </main>
   );
 }

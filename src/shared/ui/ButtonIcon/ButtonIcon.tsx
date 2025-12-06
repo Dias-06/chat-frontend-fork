5caa8cee9828ea0213c76f8eaf8468e92cf0db55
Dias-06 <d_mukashev@kbtu.kz>
2025-12-06 20:53:43 +0500

    feat: добален компонент кнопки с иконками

---
+++ src/shared/ui/ButtonIcon/ButtonIcon.tsx
@@ -0,0 +1,29 @@
+'use client'
+interface ButtonIconProps{
+    variant: 'primary' | 'border' | 'transparent' | 'inactive' | 'active' | 'red',
+    onClick: React.MouseEventHandler<HTMLButtonElement>,
+    size: 'sm' | 'lg' | 'md',
+    children: React.ReactNode,
+
+}
+const ButtonIcon = (props : ButtonIconProps) => {
+  const {variant,children,size,onClick} = props
+  const variants = {
+    primary: 'bg-primary',
+    inactive: 'bg-[#E5E4F7]',// для окна звонка (вкл.видео,вкл.звук) когда не активна
+    active: 'bg-white', //Для окна звонка когда активна
+    border: 'bg-transparent border-primary border-[1.5px]',
+    transparent: 'bg-transparent',
+    red: 'bg-red-600'
+   }
+  const sizes = {
+    sm: 'w-[24px] h-[24px]',
+    md: 'w-[36px] h-[36px]',
+    lg: 'w-[72px] h-[72px]'
+  }
+  return (
+    <button onClick={onClick} className={`flex cursor-pointer justify-center items-center rounded-full ${variants[variant]} ${sizes[size]}`} type="button" >{children}</button>
+  )
+}
+
+export default ButtonIcon

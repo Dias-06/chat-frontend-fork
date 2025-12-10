4b097d996ea2c1ff4d2d4745f5e2d23faf07d494
asuna <cherepanova.so04@gmail.com>
2025-12-10 16:42:04 +0500

    chore: почищена главная страница

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,3 @@
-"use client";
-
-import { NotificationsToggler } from "@widgets/notificationsToggler";
-import { useState } from "react";
-
 export default function Home() {
-  const [active, setActive] = useState<boolean>(true);
-
-  return (
-    <main>
-      <NotificationsToggler
-        active={active}
-        onClick={() => setActive((prev) => !prev)}
-      />
-    </main>
-  );
+  return <main></main>;
 }
